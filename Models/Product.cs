884c6b20f673326c53899b04f3418366ed63bf59
HOU <HOU@DESKTOP-M5A53QE>
2025-06-06 18:01:23 +0800

    Revert "Database First"
    This reverts commit 304aa80eaecee941a88e3c9a4e5658b129e20819.

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,33 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Interview.Models;
-
-public partial class Product
+﻿namespace Interview.Models
 {
-    public int ProductId { get; set; }
-
-    public string ProductName { get; set; } = null!;
-
-    public int? SupplierId { get; set; }
-
-    public int? CategoryId { get; set; }
-
-    public string? QuantityPerUnit { get; set; }
-
-    public decimal? UnitPrice { get; set; }
-
-    public short? UnitsInStock { get; set; }
-
-    public short? UnitsOnOrder { get; set; }
-
-    public short? ReorderLevel { get; set; }
-
-    public bool Discontinued { get; set; }
-
-    public virtual Category? Category { get; set; }
-
-    public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
-
-    public virtual Supplier? Supplier { get; set; }
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; } = String.Empty;
+        public int? SupplierID { get; set; }
+        public int? CategoryID { get; set; }
+        public string? QuantityPerUnit { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public short? UnitsInStock { get; set; }
+        public short? UnitsOnOrder { get; set; }
+        public short? ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+    }
 }
